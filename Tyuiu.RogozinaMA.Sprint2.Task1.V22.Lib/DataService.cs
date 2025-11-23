4a402b97f67e775b71b7ecf6a8e3529fef7db8d7
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-23 14:56:11 +0500

    Task1

--- Tyuiu.RogozinaMA.Sprint2.Task1.V22.Lib/DataService.cs
+++ Tyuiu.RogozinaMA.Sprint2.Task1.V22.Lib/DataService.cs
@@ -9,13 +9,12 @@ namespace Tyuiu.RogozinaMA.Sprint2.Task1.V22.Lib
         {
             bool[] res = new bool[6];
 
-            // Операции сравнения и логические операции
-            res[0] = (a < b) | (c > d);          // (324 < 696) | (254 > 155) → True | True → True
-            res[1] = (a == b) & (c != d);        // (324 == 696) & (254 != 155) → False & True → False
-            res[2] = (a <= b) || (c >= d);       // (324 <= 696) || (254 >= 155) → True || True → True
-            res[3] = (a > b) && (c < d);         // (324 > 696) && (254 < 155) → False && False → False
-            res[4] = !(a != b);                  // !(324 != 696) → !True → False
-            res[5] = (a == b) ^ (c == d);        // (324 == 696) ^ (254 == 155) → False ^ False → False
+            res[0] = (a < b) | (c > d);          // True | True → True
+            res[1] = (a != b) & (c != d);        // True & True → True  
+            res[2] = (a <= b) || (c >= d);       // True || True → True
+            res[3] = (a > b) && (c < d);         // False && False → False
+            res[4] = !(a < b);                   // !True → False
+            res[5] = (a == b) ^ (c == d);        // False ^ False → False
 
             return res;
         }
