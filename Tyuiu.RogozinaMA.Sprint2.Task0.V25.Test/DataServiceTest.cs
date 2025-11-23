dfffced62862e2de7230b950b54b6e3dd5276e05
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-23 13:56:41 +0500

    Add project files.

---
+++ Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.RogozinaMA.Sprint2.Task0.V25.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] wait = { true, true, true, false, false, false };
+            bool[] res = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsFirstOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(true, res[0]); // x == y + 46 → 205 == 205
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsSecondOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(true, res[1]); // x != y → 205 != 159
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsThirdOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(true, res[2]); // x - 100 < y → 105 < 159
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsFourthOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(false, res[3]); // x > y + 50 → 205 > 209
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsFifthOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(false, res[4]); // x <= y → 205 <= 159
+        }
+
+        [TestMethod]
+        public void ValidGetCompareOperationsSixthOperation()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] res = ds.GetCompareOperations(x, y);
+            Assert.AreEqual(false, res[5]); // x - 46 >= y + 1 → 159 >= 160
+        }
+    }
+}
---
+++ Tyuiu.RogozinaMA.Sprint2.Task0.V25/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RogozinaMA.Sprint2.Task0.V25.Lib;
+
+namespace Tyuiu.RogozinaMA.Sprint2.Task0.V25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнила: Рогозина М.А. | ИСПб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Рогозина Милана Андреевна | ИСПб-25-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться) и арифметических      *");
+            Console.WriteLine("* выражений, которая вернет логическую последовательность(массив):        *");
+            Console.WriteLine("* (True, True, True, False, False, False), при x = 205, y = 159           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 205;
+            int y = 159;
+
+            Console.WriteLine($"x = {x}");
+            Console.WriteLine($"y = {y}");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            // Получение результата через библиотеку
+            bool[] result = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("Результат сравнений:");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"Операция {i + 1}: {result[i]}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Ожидаемая последовательность: (True, True, True, False, False, False)");
+
+            Console.ReadLine();
+        }
+    }
+}
