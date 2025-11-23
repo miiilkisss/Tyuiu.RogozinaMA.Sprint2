e7d378ee86fac6245cc3f63d3e76cd892f84229f
mila.rogo@yandex.ru <mila.rogo@yandex.ru>
2025-11-23 14:38:35 +0500

    Task0

--- Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test/DataServiceTest.cs
+++ Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test/DataServiceTest.cs
@@ -1,10 +1,21 @@
-﻿namespace Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.RogozinaMA.Sprint2.Task0.V25.Lib;
 
-[TestClass]
-public sealed class DataServiceTest
+namespace Tyuiu.RogozinaMA.Sprint2.Task0.V25.Test
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class DataServiceTest
     {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 205;
+            int y = 159;
+            bool[] wait = { true, true, true, false, false, false };
+            bool[] res = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, res);
+        }
     }
 }
